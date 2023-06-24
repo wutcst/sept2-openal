8d20fb28dd05b44ae96d2beecb06383757801fe2
fakeasmile <2300893881@qq.com>
2023-06-24 17:31:00 +0800

    增加MVC开发模式下需要的包

---
+++ src/test/java/sprintboot_adventure/SprintbootAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package sprintboot_adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
